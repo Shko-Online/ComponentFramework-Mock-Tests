8b37686fc1f34e1e590960527383d66672ceab0c
Betim Beja <11160171+BetimBeja@users.noreply.github.com>
2022-09-11 14:37:39 +0200

    improved coverage

--- __tests__/LookupSimpleControl.test.ts
+++ __tests__/LookupSimpleControl.test.ts
@@ -1,34 +1,51 @@
 import * as sinon from 'sinon';
 
 import { ComponentFrameworkMockGenerator } from '@shko-online/componentframework-mock/ComponentFramework-Mock-Generator';
-import { LookupSimpleControl }  from '@powerapps-samples/lookup-simple-control/LookupSimpleControl';
-import { IInputs, IOutputs }  from '@powerapps-samples/lookup-simple-control/LookupSimpleControl/generated/ManifestTypes';
+import { LookupSimpleControl } from '@powerapps-samples/lookup-simple-control/LookupSimpleControl';
+import { IInputs, IOutputs } from '@powerapps-samples/lookup-simple-control/LookupSimpleControl/generated/ManifestTypes';
 import { LookupPropertyMock } from '@shko-online/componentframework-mock/ComponentFramework-Mock/PropertyTypes/LookupProperty.mock';
 
 describe("ControlStateAPI tests", () => {
-    let mockGenerator: ComponentFrameworkMockGenerator<IInputs,IOutputs>;
-    beforeEach(()=>{
+    let mockGenerator: ComponentFrameworkMockGenerator<IInputs, IOutputs>;
+    beforeEach(() => {
         const container = document.createElement('div');
 
-         mockGenerator = new ComponentFrameworkMockGenerator(
+        mockGenerator = new ComponentFrameworkMockGenerator(
             LookupSimpleControl,
             {
-               controlValue: LookupPropertyMock,
-               controlValue1: LookupPropertyMock
+                controlValue: LookupPropertyMock,
+                controlValue1: LookupPropertyMock
             },
             container);
         document.body.appendChild(container);
     }
     )
 
-    afterEach(()=>{
+    afterEach(() => {
         document.body.innerHTML = null;
     })
 
-    it("Init should work", () => {            
+    it("Init should work", () => {
         mockGenerator.ExecuteInit();
         sinon.assert.calledOnce(mockGenerator.control.init);
         expect(document.body).toMatchSnapshot();
     })
 
+    it("Update View should Work", () => {
+        mockGenerator.context.parameters.controlValue.raw = [{
+            entityType: 'mocked_entity',
+            id: "00000000-0000-0000-0000-000000000001",
+            name: 'Test Betim'
+        }]
+        mockGenerator.context.parameters.controlValue1.raw = [{
+            entityType: 'mocked_entity',
+            id: "00000000-0000-0000-0000-000000000002",
+            name: 'Test Betim2'
+        }]
+        mockGenerator.ExecuteInit();
+        mockGenerator.ExecuteUpdateView();
+        sinon.assert.calledOnce(mockGenerator.control.init);
+        sinon.assert.calledOnce(mockGenerator.control.updateView);
+        expect(document.body).toMatchSnapshot();
+    })
 });
