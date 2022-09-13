3e4b675ac1adc9cef94dfa161dbfe57334ee946e
asllanmakaj7 <107688754+asllanmakaj7@users.noreply.github.com>
2022-09-13 16:57:33 +0200

    NavigationApi tests

--- jest.config.js
+++ jest.config.js
@@ -24,7 +24,8 @@ module.exports = {
       '@powerapps-samples/increment-control/(.*)': '<rootDir>/PowerApps-Samples/component-framework/IncrementControl/$1',
       '@powerapps-samples/linear-input-control/(.*)': '<rootDir>/PowerApps-Samples/component-framework/LinearInputControl/$1',
       '@powerapps-samples/localization-api-control/(.*)': '<rootDir>/PowerApps-Samples/component-framework/LocalizationAPIControl/$1',
-      '@powerapps-samples/lookup-simple-control/(.*)': '<rootDir>/PowerApps-Samples/component-framework/LookupSimpleControl/$1'
+      '@powerapps-samples/lookup-simple-control/(.*)': '<rootDir>/PowerApps-Samples/component-framework/LookupSimpleControl/$1',
+      '@powerapps-samples/navigation-api-control/(.*)': '<rootDir>/PowerApps-Samples/component-framework/NavigationAPIControl/$1'
     },  
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
