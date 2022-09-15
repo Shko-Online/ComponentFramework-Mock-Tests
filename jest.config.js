2b42fdb60ea65db1cf3ab1f86f598d8245bf59aa
asllanmakaj7 <107688754+asllanmakaj7@users.noreply.github.com>
2022-09-15 11:17:45 +0200

    propertyset table

--- jest.config.js
+++ jest.config.js
@@ -29,7 +29,8 @@ module.exports = {
       '@powerapps-samples/navigation-api-control/(.*)': '<rootDir>/PowerApps-Samples/component-framework/NavigationAPIControl/$1',
       '@powerapps-samples/device-api-control/(.*)': '<rootDir>/PowerApps-Samples/component-framework/DeviceApiControl/$1',
       '@powerapps-samples/web-api-control/(.*)': '<rootDir>/PowerApps-Samples/component-framework/webAPIControl/$1',
-      '@powerapps-samples/i-frame-control/(.*)': '<rootDir>/PowerApps-Samples/component-framework/IFrameControl/$1'
+      '@powerapps-samples/i-frame-control/(.*)': '<rootDir>/PowerApps-Samples/component-framework/IFrameControl/$1',
+      '@powerapps-samples/property-set-table-control/(.*)': '<rootDir>/PowerApps-Samples/component-framework/PropertySetTableControl/$1'
     },  
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
