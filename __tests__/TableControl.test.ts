529933b481d6656d5f0d995431d4e2d675a35898
Betim Beja <11160171+BetimBeja@users.noreply.github.com>
2022-10-06 06:39:35 +0200

    removed console logs

--- __tests__/TableControl.test.ts
+++ __tests__/TableControl.test.ts
@@ -16,7 +16,7 @@
 import * as sinon from 'sinon';
 
 import { ComponentFrameworkMockGenerator } from '@shko-online/componentframework-mock/ComponentFramework-Mock-Generator';
-import { TableControl }  from '@powerapps-samples/table-control/TableControl';
+import { TableControl } from '@powerapps-samples/table-control/TableControl';
 import { IInputs, IOutputs } from '@powerapps-samples/table-control/TableControl/generated/ManifestTypes';
 import * as resource from '@powerapps-samples/table-control/TableControl/strings/TableControl.1033.resx';
 import { StringPropertyMock } from '@shko-online/componentframework-mock/ComponentFramework-Mock/PropertyTypes/StringProperty.mock';
@@ -35,96 +35,91 @@ describe("TableControl", () => {
 			container);
 		mockGenerator.SetControlResource(resource);
 		document.body.appendChild(container);
-		mockGenerator.context.utils.getEntityMetadata.callsFake((entity)=>{
-		return new	Promise<ComponentFramework.PropertyHelper.EntityMetadata>(resolve=>{
-			resolve({
-				data:  [
-					{
-						entityName: 'account'
-					}
-				]
+		mockGenerator.context.utils.getEntityMetadata.callsFake((entity) => {
+			return new Promise<ComponentFramework.PropertyHelper.EntityMetadata>(resolve => {
+				resolve({
+					data: [
+						{
+							entityName: 'account'
+						}
+					]
+				})
 			})
 		})
-		})
 		mockGenerator.context.utils.lookupObjects.callsFake((lookupOptions: ComponentFramework.UtilityApi.LookupOptions) => {
-            return new Promise<ComponentFramework.LookupValue[]>((resolve) => {
-                resolve([{
-                    entityType: lookupOptions.entityTypes ? lookupOptions.entityTypes[0] : 'account',
-                    id: "00000000-0000-0000-0000-000000000001",
-                    name: "Account",
-                }])
-            });
-        })
-		
+			return new Promise<ComponentFramework.LookupValue[]>((resolve) => {
+				resolve([{
+					entityType: lookupOptions.entityTypes ? lookupOptions.entityTypes[0] : 'account',
+					id: "00000000-0000-0000-0000-000000000001",
+					name: "Account",
+				}])
+			});
+		})
+
 	})
 
 	afterEach(() => {
 		document.body.innerHTML = null;
 	})
-    it("Init should work", () => {
+	it("Init should work", () => {
 		mockGenerator.ExecuteInit();
 		sinon.assert.calledOnce(mockGenerator.control.init);
 		expect(document.body).toMatchSnapshot();
 	})
-    it("Update View should Work", () => {    
-        mockGenerator.ExecuteInit();
-        mockGenerator.ExecuteUpdateView();
-        sinon.assert.calledOnce(mockGenerator.control.init);
-        sinon.assert.calledOnce(mockGenerator.control.updateView);
-        expect(document.body).toMatchSnapshot();
-    })
-    it("GetOutputs should work", () => {
-        mockGenerator.control.getOutputs();
+	it("Update View should Work", () => {
 		mockGenerator.ExecuteInit();
 		mockGenerator.ExecuteUpdateView();
-		expect(document.body).toMatchSnapshot(); 
+		sinon.assert.calledOnce(mockGenerator.control.init);
+		sinon.assert.calledOnce(mockGenerator.control.updateView);
+		expect(document.body).toMatchSnapshot();
+	})
+	it("GetOutputs should work", () => {
+		mockGenerator.control.getOutputs();
+		mockGenerator.ExecuteInit();
+		mockGenerator.ExecuteUpdateView();
+		expect(document.body).toMatchSnapshot();
 	})
 	it("Destroy should work", () => {
-        mockGenerator.control.destroy();
+		mockGenerator.control.destroy();
 		mockGenerator.ExecuteInit();
 		mockGenerator.ExecuteUpdateView();
-		expect(document.body).toMatchSnapshot(); 
+		expect(document.body).toMatchSnapshot();
 	})
-  
-    it("Click should Work", () => {
-		
+
+	it("Click should Work", () => {
+
 		mockGenerator.ExecuteInit();
 		mockGenerator.ExecuteUpdateView();
 		expect(document.body).toMatchSnapshot();
 
 		const buttons = mockGenerator.container.querySelectorAll("button");
-		buttons.forEach((button) =>{
-        var evt = document.createEvent("Event");
-		evt.initEvent("click", true, false);
-		button.dispatchEvent(evt);
-		console.log(button.type);
+		buttons.forEach((button) => {
+			var evt = document.createEvent("Event");
+			evt.initEvent("click", true, false);
+			button.dispatchEvent(evt);
 
-		mockGenerator.ExecuteUpdateView();
-		expect(document.body).toMatchSnapshot();
-        })
-     
+			mockGenerator.ExecuteUpdateView();
+			expect(document.body).toMatchSnapshot();
+		})
+
 	})
-   
+
 	it("onlookup", () => {
-		
+
 		mockGenerator.ExecuteInit();
 		mockGenerator.ExecuteUpdateView();
 		expect(document.body).toMatchSnapshot();
-		const entitymeta =mockGenerator.context.utils.getEntityMetadata("account");
-        const times=0;
+		const entitymeta = mockGenerator.context.utils.getEntityMetadata("account");
+		const times = 0;
 		const buttons = mockGenerator.container.querySelectorAll(".SampleControlHtmlTable_ButtonClass");
-		buttons.forEach((button) =>{
-			console.log(button);
-			console.log(times);
-        var evt = document.createEvent("Event");
-		evt.initEvent("click", true, false);
-		button.dispatchEvent(evt);
-		times+1;
+		buttons.forEach((button) => {
+			var evt = document.createEvent("Event");
+			evt.initEvent("click", true, false);
+			button.dispatchEvent(evt);
+			times + 1;
 
-		mockGenerator.ExecuteUpdateView();
-		expect(document.body).toMatchSnapshot();
+			mockGenerator.ExecuteUpdateView();
+			expect(document.body).toMatchSnapshot();
+		})
 	})
-     
-	})
-    
 })
